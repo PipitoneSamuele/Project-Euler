82b944063150f1b1be6d86c9c58fecf9a4061fb4
PipitoneSamuele <samuele.pipitone@gmail.com>
2022-06-11 19:10:34 +0200

    problem 1 and 2

--- src/implementations/Solutions.java
+++ src/implementations/Solutions.java
@@ -1,7 +1,40 @@
 package implementations;
 
 public class Solutions {
+	
+	final int MAXFIBONACCI = 4000000;
 
 	public Solutions() {}
+	
+	//1)------------------------------------------------------------------------------------------------------------------
+	public int multiplesOf3And5(int n) {
+		
+		int count = 0;
+		
+		//bruteforce
+		for(int i = 3; i < n; i++) {
+			if(i % 3 == 0 || i % 5 == 0) {
+				count += i;
+			}
+		}
+		return count;
+	}
+	
+	//2) ----------------------------------------------------------------------------------------------------------------
+	public int evenFibonacci(int fibonacci, int fibonacciPrev, int fibonacciHold, int sum) {
+		
+		if(fibonacci > MAXFIBONACCI) {
+			return sum;
+		}
+		
+		if(fibonacci % 2 == 0) {
+			sum += fibonacci;
+		}
+		
+		fibonacciHold = fibonacci;
+		
+		return evenFibonacci((fibonacci+fibonacciPrev), fibonacciHold, 0, sum);
+	}
+	
 	
 }
--- src/test_results/Main.java
+++ src/test_results/Main.java
@@ -7,7 +7,12 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Solutions solution = new Solutions();
-
+	
+		//1) Multiples of 3 and 5
+		System.out.println("1) " + solution.multiplesOf3And5(1000) + "\n"); 
+		
+		//2) Even Fibonacci numbers
+		System.out.println("2) " + solution.evenFibonacci(1, 0, 0, 0) + "\n"); 
 	}
 
 }
